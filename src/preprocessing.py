cab15c26ba179ec4716d69c27698b70004b77efd
Marwan <marwanelgharbawy2@gmail.com>
2026-09-05 17:47:23 +0300

    Remove segmentation from preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,23 +5,24 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
     nyq = 0.5 * fs
     low = lowcut / nyq
     high = highcut / nyq
-    b, a = signal.butter(order, [low, high], btype='band')
-    return signal.filtfilt(b, a, data)
+    sos = signal.butter(order, [low, high], btype='band', output='sos')
+    return signal.sosfiltfilt(sos, data)
 
 def preprocess_signals(ecg_raw, pcg_raw):
-    # 1. Filtering
+    # Filtering
     # ECG: 0.5 to 40 Hz (removes baseline wander and high-freq noise)
     ecg_filtered = butter_bandpass_filter(ecg_raw, 0.5, 40.0, fs=500)
     
     # PCG: 25 to 400 Hz (focuses on main heart sound frequencies)
     pcg_filtered = butter_bandpass_filter(pcg_raw, 25.0, 400.0, fs=4000)
     
-    # 2. Normalization (Z-score standardization)
+    # Normalization (Z-score standardization)
     ecg_norm = (ecg_filtered - np.mean(ecg_filtered)) / np.std(ecg_filtered)
     pcg_norm = (pcg_filtered - np.mean(pcg_filtered)) / np.std(pcg_filtered)
     
     return ecg_norm, pcg_norm
 
+# Not used for now
 def segment_signals(ecg, pcg, segment_length_sec=5):
     # 5 seconds = 2500 samples (ECG), 20000 samples (PCG)
     ecg_samples = segment_length_sec * 500
